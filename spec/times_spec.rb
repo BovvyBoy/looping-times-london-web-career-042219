d9e9a863938ff621661113455ac7f0c0942bd4fe
Daniel Bovington <d.bovington@gmail.com>
2019-03-16 00:33:33 +0000

    Automatically backed up by Learn

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -1,6 +1,10 @@
 require_relative "spec_helper"
 require_relative "../times.rb"
 
+def using_times(looping_string)
+  
+end
+
 describe "#using_times" do 
 	it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
 
